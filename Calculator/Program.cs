859446468b108471fb9c015e97e9baa9c11c7fbe
Emily Patsko <emily.patsko@softwire.com>
2022-07-03 15:39:41 +0100

    Add ncalc and ask user to input operator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,13 +1,17 @@
-﻿namespace Calculator
+﻿using NCalc;
+
+namespace Calculator
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the calculator!");
+            var op = GetOperator("Please enter the operator");
             var number1 = GetInputNumber("Please enter the first number");
             var number2 = GetInputNumber("Please enter the second number");
-            Console.WriteLine($"{number1} x {number2} = {number1 * number2}");
+            var expression = new Expression($"{number1}{op}{number2}");
+            Console.WriteLine($"{number1} {op} {number2} = {expression.Evaluate()}");
         }
 
         static double GetInputNumber(string inputMessage, string errorMessage = "You must enter a valid number, please try again.")
@@ -23,6 +27,25 @@
                 catch (Exception e)
                 {
                     Console.WriteLine(errorMessage);
+                }
+            }
+        }
+
+        static string GetOperator(string inputMessage)
+        {
+            var operators = new[] {"+", "-", "*", "/"};
+            while (true)
+            {
+                try
+                {
+                    Console.Write($"{inputMessage}: ");
+                    var input = Console.ReadLine()!.Trim();
+                    if (!operators.Contains(input)) throw new Exception();
+                    return input;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("You must enter one of +, -, *, /. Please try again.");
                 }
             }
         }
