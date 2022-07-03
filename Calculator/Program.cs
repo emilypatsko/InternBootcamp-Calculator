b745ee3b43450d84111df5365b45fc288de7a856
Emily Patsko <emily.patsko@softwire.com>
2022-07-03 15:09:11 +0100

    Add a simple multiplier

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,6 +5,26 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the calculator!");
+            var number1 = GetInputNumber("Please enter the first number");
+            var number2 = GetInputNumber("Please enter the second number");
+            Console.WriteLine($"{number1} x {number2} = {number1 * number2}");
+        }
+
+        static double GetInputNumber(string inputMessage, string errorMessage = "You must enter a valid number, please try again.")
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write($"{inputMessage}: ");
+                    var input = Console.ReadLine();
+                    return Double.Parse(input);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(errorMessage);
+                }
+            }
         }
     }
 }
